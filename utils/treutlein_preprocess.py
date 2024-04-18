0895b085f2cad101f52d90bc7621ff546fd89971
Sebastian Damrich <sebastian.damrich@gmx.de>
2024-04-18 14:28:59 +0200

    restructures some utils. Adds the get_computed_spec_params function to utils/utils along with download capabilities for the Kanton dataset and metric loading. Also adds require preprocessing code for the Kanton dataste. Furthermore, adds utils for computing the metrics in metrics_utils.py.

---
+++ utils/treutlein_preprocess.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Treutlein lab ape organoid data
+# The data are here: https://www.ebi.ac.uk/arrayexpress/experiments/E-MTAB-7552/
+#
+# Download files 1 to 7
+# https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-7552/E-MTAB-7552.processed.1.zip
+# https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-7552/E-MTAB-7552.processed.2.zip
+# https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-7552/E-MTAB-7552.processed.3.zip
+# https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-7552/E-MTAB-7552.processed.4.zip
+# https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-7552/E-MTAB-7552.processed.5.zip
+# https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-7552/E-MTAB-7552.processed.6.zip
+# https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-7552/E-MTAB-7552.processed.7.zip
+# and unpack
+#
+# Download supplementary informationfrom the Nature paper https://static-content.springer.com/esm/art%3A10.1038%2Fs41586-019-1654-9/MediaObjects/41586_2019_1654_MOESM3_ESM.zip, extract `Supplementary_Table_5.txt` and rename it into `metadata_chimp_cells_suppl.tsv`. The metadata file on arrayexpress seems to be wrong. I wrote to the authors to clarify.
+
+# from https://github.com/berenslab/ne-spectrum/blob/master/static/treutlein-preprocess.py
+
+import numpy as np
+import pandas as pd
+import scipy
+
+from scipy.io import mmread
+from pathlib import Path
+
+import vis_utils.rnaseqTools as rnaseqTools
+
+
+def preprocess(metafile, countfile, line, n=1000, decay=1.5, n_components=50):
+    meta = pd.read_csv(metafile, sep="\t")
+
+    counts = mmread(str(countfile))
+    counts = scipy.sparse.csc_matrix(counts).T
+
+    ind = meta["in_FullLineage"].values
+    if line is not None:
+        ind = ind & (meta["Line"].values == line)
+
+    seqDepths = np.array(counts[ind, :].sum(axis=1))
+    stage = meta["Stage"].values[ind].astype("str")
+
+    impGenes = rnaseqTools.geneSelection(counts[ind, :], n=n, decay=decay, plot=False)
+
+    # Transformations
+    X = np.log2(np.array(counts[:, impGenes][ind, :]) / seqDepths * np.median(seqDepths) + 1)
+    X = X - X.mean(axis=0)
+    U, s, V = np.linalg.svd(X, full_matrices=False)
+    X = np.dot(U, np.diag(s))
+    X = X[:, np.argsort(s)[::-1]][:, :n_components]
+
+    return X, stage
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        "Preprocess developmental data.  This requires the data to be "
+        "downloaded and unzipped already, prior to running this.  Read "
+        "the comments at the top of this file in order to get the links "
+        " that point to the data files and some further instructions."
+    )
+    parser.add_argument(
+        "--type",
+        default="chimp",
+        help="Whether chimp or human data should be extracted",
+    )
+    parser.add_argument("--line", default=None, help="Which line should be saved")
+    parser.add_argument(
+        "--prefix",
+        type=Path,
+        default="~/Downloads/treutlein",
+        help="The location of the raw data",
+    )
+    parser.add_argument(
+        "--outfmt", default="npy", help="Whether to output npy or pickle files"
+    )
+
+    args = parser.parse_args()
+
+    # select a default line for human
+    if args.type == "human" and (args.line is None):
+        args.line = "409b2"
+
+    if args.type == "chimp":
+        metafile = "metadata_chimp_cells_suppl.tsv"
+        countfile = "chimp_cell_counts_consensus.mtx"
+        line = None
+    elif args.type == "human":
+        metafile = "metadata_human_cells.tsv"
+        countfile = "human_cell_counts_consensus.mtx"
+        line = args.line
+
+    if line not in [None, "409b2", "H9"]:
+        raise ValueError(f"Value for line {line} is not valid.")
+
+    metafile = args.prefix.expanduser() / metafile
+    countfile = args.prefix.expanduser() / countfile
+    X, stage = preprocess(metafile, countfile, line)
+
+    outputfile = "{}{}".format(
+        args.type, "" if args.line is None else f"-{args.line.lower()}"
+    )
+
+    if args.outfmt == "pickle":
+        import pickle
+
+        pickle.dump([X, stage], open(outputfile + ".pickle", "wb"))
+    elif args.outfmt == "npy":
+        np.save(outputfile + ".data.npy", X)
+        np.save(outputfile + ".labels.npy", stage)
--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,11 @@
 from pkg_resources import resource_stream
 import numpy as np
-from tsne_slider import TSNESpectrum, CNESpectrum
+from ne_spectrum import TSNESpectrum, CNESpectrum
+import os
+import requests
+import pandas as pd
+import pickle
+from .treutlein_preprocess import preprocess as treut_preprocess
 
 def get_path(path_type):
     #  util for reading in paths from file
@@ -28,16 +33,151 @@ def get_path(path_type):
             print("There is no path 'style_path'.")
 
 
-def get_close_spec_params(selected_spec_params, spectrum_via):
+def get_computed_spec_params(spectrum_via, min_spec_param=None, max_spec_param=None):
+
+    # use only those spec param bounds that are provided and resort to the defaults in of ne_spectum instead.
+    kwargs = {}
+    if min_spec_param is not None:
+        kwargs["min_spec_param"] = min_spec_param
+    if max_spec_param is not None:
+        kwargs["max_spec_param"] = max_spec_param
+
+    # create the spectrum object to obtain the intermediate spec params
     if spectrum_via == "tsne":
-        spectrum = TSNESpectrum()
+        spectrum = TSNESpectrum(**kwargs)
     elif spectrum_via == "cne":
-        spectrum = CNESpectrum()
+        spectrum = CNESpectrum(**kwargs)
     else:
         raise ValueError("spectrum_via must be either 'tsne' or 'cne'")
+
     l = spectrum.kwarg_list
     computed_spec_params = np.array([l[i][spectrum.spectrum_param_name] for i in range(len(l))])
+
+    return computed_spec_params
+
+
+def get_close_spec_params(selected_spec_params, spectrum_via, min_spec_param=None, max_spec_param=None):
+
+    computed_spec_params = get_computed_spec_params(spectrum_via=spectrum_via,
+                                                    min_spec_param=min_spec_param,
+                                                    max_spec_param=max_spec_param)
+
     dists = np.abs(computed_spec_params[:, None] - selected_spec_params[None])
     idx = np.argmin(dists, axis=0)
     close_spec_params = computed_spec_params[idx]
     return close_spec_params, idx, computed_spec_params
+
+
+
+def download_file(url, destination):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(destination, 'wb') as f:
+            f.write(response.content)
+        print(f"File downloaded successfully from {url}.")
+    else:
+        print(f"Failed to download file from {url}. Status code: {response.status_code}")
+
+def save_dict(di_, filename_):
+    with open(filename_, 'wb') as f:
+        pickle.dump(di_, f)
+
+def load_dict(filename_):
+    with open(filename_, 'rb') as f:
+        ret_di = pickle.load(f)
+    return ret_di
+
+def load_human(root_path):
+    root_path = os.path.join(root_path, "human-409b2")
+    if not os.path.exists(root_path):
+        os.mkdir(root_path)
+
+    try:
+        x = np.load(os.path.join(root_path, "human-409b2.data.npy"))
+        y = np.load(os.path.join(root_path, "human-409b2.labels.npy"))
+        d = load_dict(os.path.join(root_path, "human-409b2.pkl"))
+    except FileNotFoundError:
+
+        metafile = os.path.join(root_path,
+                                #"unzipped_files",
+                                "metadata_human_cells.tsv")
+        countfile = os.path.join(root_path,
+                                 #"unzipped_files",
+                                 "human_cell_counts_consensus.mtx")
+        urls = []
+        if not os.path.exists(metafile):
+            urls.append("https://www.ebi.ac.uk/biostudies/files/E-MTAB-7552/metadata_human_cells.tsv")
+        if not os.path.exists(countfile):
+            urls.append("https://www.ebi.ac.uk/biostudies/files/E-MTAB-7552/human_cell_counts_consensus.mtx")
+
+        if len(urls) > 0:
+            print("Downloading data")
+        for url in urls:
+            filename = os.path.join(root_path, url.split("/")[-1])
+            download_file(url, filename)
+
+        print("Preprocessing data")
+        line = "409b2"
+        X, stage = treut_preprocess(metafile, countfile, line)
+
+        outputfile = "human-409b2"
+
+        np.save(os.path.join(root_path, outputfile + ".data.npy"), X)
+        np.save(os.path.join(root_path, outputfile + ".labels.npy"), stage)
+        x = X
+        y = stage
+
+        print("Done")
+
+        # meta data
+        d = {"label_colors": {
+            "iPSCs": "navy",
+            "EB": "royalblue",
+            "Neuroectoderm": "skyblue",
+            "Neuroepithelium": "lightgreen",
+            "Organoid-1M": "gold",
+            "Organoid-2M": "tomato",
+            "Organoid-3M": "firebrick",
+            "Organoid-4M": "maroon",
+        }, "time_colors": {
+            "  0 days": "navy",
+            "  4 days": "royalblue",
+            "10 days": "skyblue",
+            "15 days": "lightgreen",
+            "  1 month": "gold",
+            "  2 months": "tomato",
+            "  3 months": "firebrick",
+            "  4 months": "maroon",
+        }}
+
+        # cluster assignments
+        meta = pd.read_csv(metafile, sep="\t")
+        mask = (meta["Line"] == "409b2")* meta["in_FullLineage"]
+
+        d["clusters"] = list(meta[mask]["cl_FullLineage"])
+
+        d["color_to_time"] = {v: k for k, v in d["time_colors"].items()}
+
+        save_dict(d, os.path.join(root_path, f"{outputfile}.pkl"))
+
+    d["clusters"] = np.array(d["clusters"])
+    return x, y, d
+
+
+def load_metrics(spectrum_via, root_path, dataset, n_slides, sub_sample=0, min_spec_param=None, max_spec_param=None):
+    k = 15
+
+    computed_spec_params = get_computed_spec_params(spectrum_via, min_spec_param, max_spec_param)
+
+    min_spec_param = np.round(computed_spec_params.min(), 2)
+    max_spec_param = np.round(computed_spec_params.max(), 2)
+
+    file_name_recalls = f"human-409b2_{spectrum_via}_n_slides_{n_slides}_min_spec_{min_spec_param}_max_spec_{max_spec_param}_recalls_k_{k}.npy"
+
+    recalls = np.load(os.path.join(root_path, dataset, file_name_recalls))
+
+    file_name_spears = f"human-409b2_{spectrum_via}_n_slides_{n_slides}_min_spec_{min_spec_param}_max_spec_{max_spec_param}_spear_sum_sample_{sub_sample}.npy"
+
+    spears = np.load(os.path.join(root_path, dataset, file_name_spears))
+
+    return recalls, spears
